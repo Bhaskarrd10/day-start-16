e1cfc86ddee8a23cc7b23c9c7c1ef6df98a6f983
bhaskarreddy <bhaskrrd10@gmail.com>
2022-05-16 14:29:31 +0530

    Adding Prettytable package

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from turtle import Turtle, Screen
+#from turtle import Turtle, Screen
+#
+#timmy = Turtle()
+#print(timmy)
+#timmy.shape("turtle")
+#timmy.color("coral")
+#timmy.forward(100)
+#
+#
+#my_screen = Screen()
+#print(my_screen.canvheight)
+#my_screen.exitonclick()
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column("Pokename", ["Pikachu", "Sguirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water","Fire"])
+print(table)
+
+
+
